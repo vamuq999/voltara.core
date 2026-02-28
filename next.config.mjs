1a348f8416dee9b835094fba4bc523652878948c
vamuq999 <vamuq999@gmail.com>
2026-02-28 12:53:14 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
-export default nextConfig;
+export default config;
